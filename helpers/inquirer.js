cb79ffd9db0494d59729877853b593e737711011
alfonsoj-entwickler <alfonsoj.entwickler@gmail.com>
2023-06-12 20:13:03 +0200

    [TASK] create a helper class to create menus

---
+++ helpers/inquirer.js
@@ -0,0 +1,115 @@
+import inquirer from "inquirer";
+import colors from "colors";
+
+const menuOpts = [
+  {
+    type: "list",
+    name: "option",
+    message: "What do you want to do?",
+    choices: [
+      { value: 1, name: `${'1.'.white} Search a city` },
+      { value: 2, name: `${'2.'.white} History` },
+      { value: 0, name: `${'0.'.red} Leave` },
+    ],
+  },
+];
+
+const pauseOpts = [
+  {
+    type: "input",
+    name: "key",
+    message: `Press ${'ENTER'.green} to continue`,
+  },
+];
+
+const menuDeleteTaks = [
+  {
+    type: "list",
+    name: "option",
+    message: "Select a task to delete",
+    choices: [
+      { value: 1, name: `${'1.'.white} Create task` },
+      { value: 2, name: `${'2.'.white} List Tasks` }, 
+    ],
+  },
+];
+
+const inquirerMenu = async () => {
+  console.clear();
+  console.log("===========================".green);
+  console.log("   Select a option".white);
+  console.log("===========================\n".green);
+
+  const { option } = await inquirer.prompt(menuOpts);
+
+  return option;
+};
+
+const pause = async () => {
+  const { option } = await inquirer.prompt(pauseOpts);
+
+  return option;
+};
+
+const readInput = async( message ) => {
+  const question = [
+    {
+      type: 'input',
+      name: 'desc',
+      message,
+      validate( value ) {
+        if( value.length === 0 ){
+          return 'Please enter a value'
+        }
+        else {
+          return true;
+        }
+      }
+    }
+  ];
+
+  const { desc } = await inquirer.prompt(question);
+
+  return desc;
+}
+
+const listPlaces = async( places = [] ) => {
+ 
+  const choices = places.map( (place, i) => {
+    const idx = `${i+1}`.green;
+    return {
+      value: place.id,
+      name: `${idx}. ${place.name}`,
+    }
+  });
+  choices.unshift({
+      value: 0,
+      name: '0.'.green + ' Cancel',
+  });
+  const questions = [
+    {
+      type: 'list',
+      name: 'id',
+      message: 'Select a place',
+      choices
+    }
+  ]
+  const { id } = await inquirer.prompt(questions);
+  return id;
+}
+
+const confirmation = async ( message ) => {
+  const question = [
+    {
+      type: 'confirm',
+      name: 'okay',
+      message
+    }
+  ];
+
+  const { okay } = await inquirer.prompt(question);
+
+  return okay;
+}
+
+export { inquirerMenu, pause, readInput, listPlaces, confirmation };
