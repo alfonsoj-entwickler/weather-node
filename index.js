1bc2615fcca5def06f7c19c214d572211a82893c
alfonsoj-entwickler <alfonsoj.entwickler@gmail.com>
2023-06-15 20:00:49 +0200

    [TASK] show info and history in shell

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 import colors from "colors";
 import dotenv from "dotenv";
 import { readInput, inquirerMenu, pause, listPlaces } from "./helpers/inquirer.js";
-import { Search } from "./models/seaarch.js";
+import { Search } from "./models/search.js";
 
 const main = async() => {
     // init dotenv config
@@ -18,8 +18,13 @@ const main = async() => {
                 const place = await readInput('City: ');
                 const places = await search.city(place);
                 const selectId = await listPlaces(places);
+                
+                if( selectId === '0' ) continue;
                 const selectPlace = places.find( place => place.id === selectId );
 
+                search.addHistory(selectPlace.name);
+
+                const selectWeather = await search.weatherCity(selectPlace.lat, selectPlace.long);
                 // Search this place
 
                 // Select the place
@@ -27,22 +32,29 @@ const main = async() => {
                 // Weather
 
                 // Show result
-                console.log('\nInformation about this place: '.green);
-                console.log('City: ',selectPlace.name);
-                console.log('',selectPlace.lng);
-                console.log('',selectPlace.lat);
-                console.log('',);
-                console.log('',);
-                console.log('',);
-                console.log('',);
+                if( selectPlace ) {
+                    console.log('\nInformation about this place: '.green);
+                    console.log('City: ', selectPlace.name);
+                    console.log('Descp: ', selectWeather.desc);
+                    console.log('Long: ', selectPlace.long);
+                    console.log('Lat: ', selectPlace.lat);
+                    console.log('Temp: ', selectWeather.temp);
+                    console.log('Min: ', selectWeather.min);
+                    console.log('Max: ', selectWeather.max);
+                }
+                
                 break;
         
             case 2:
+                search.capitalizeHistory.forEach( (place, i) => {
+                    const idx = `${i+1}.`.green;
+                    console.log(`${idx} ${place}`);
+                } )
                 break;
         }
 
         
-        console.log(opt);
+ 
         if( opt !== 0 )await pause();
     } while (opt !== 0);
 }
